61bda8ec25c1bf79b0691d562ae35b3a49e430c9
kolatham <92490383+kolatham@users.noreply.github.com>
2022-01-29 15:53:10 -0500

    Fixed the seeds

--- db/seed.sql
+++ db/seed.sql
@@ -6,9 +6,11 @@ VALUES ("Sales"),
        ;
 
 INSERT INTO role(title, salary, department_id)
-VALUES ("Accountant", 125000, 4),
-       ("Salesperson", 80000, 1),
+VALUES ("Salesperson", 80000, 1),
+       ("Lead Engineer", 150000, 2),
        ("Software Engineer", 120000, 2),
+       ("Account Manager", 160000, 4),
+       ("Accountant", 125000, 4)
        ("Legal Team Lead", 250000, 3),
        ("Lawyer", 190000, 3);
 
@@ -16,9 +18,9 @@ INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES ("John", "Doe", 1, NULL),
         ("Mike", "Chan", 2, 1),
        ("Ashley", "Rodriguez" 3, NULL),
-       ("Kevin", "Tupik", 3, 3),
-       ("Kumal", "Singh", 1, NULL),
-       ("Malia", "Brown", 1, 5),
-       ("Sarah", "Lourd", 4, NULL),
-       ("Tom","Allen", 5, 7)
+       ("Kevin", "Tupik", 4, 3),
+       ("Kumal", "Singh", 5, NULL),
+       ("Malia", "Brown", 6, 5),
+       ("Sarah", "Lourd", 7, NULL),
+       ("Tom","Allen", 8, 7)
        ;
